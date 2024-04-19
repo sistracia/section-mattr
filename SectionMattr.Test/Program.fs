97c199d5b196800d4edac003ddaefa5d2b3f0b59
sistracia <156994197+sistracia@users.noreply.github.com>
2024-04-19 20:58:56 +0700

    Init project

---
+++ SectionMattr.Test/Program.fs
@@ -0,0 +1,12 @@
+﻿open Expecto
+open SectionMattr
+
+[<Tests>]
+let tests =
+    test "A simple test" {
+        let subject = Say.hello "World"
+        Expect.equal subject "Hello World" "The strings should equal"
+    }
+
+[<EntryPoint>]
+let main args = runTestsWithCLIArgs [] args tests
---
+++ SectionMattr/Library.fs
@@ -0,0 +1,4 @@
+﻿namespace SectionMattr
+
+module Say =
+    let hello name = sprintf "Hello %s" name
