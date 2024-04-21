548e36be596f50518aa935fe8e2a74a1799ede1a
sistracia <156994197+sistracia@users.noreply.github.com>
2024-04-21 21:44:57 +0700

    Implement (section-matter)[https://github.com/jonschlinkert/section-matter] in F#

--- SectionMattr.Test/Program.fs
+++ SectionMattr.Test/Program.fs
@@ -1,12 +1,177 @@
 ﻿open Expecto
 open SectionMattr
+open System.IO
+open System.Text.Json
+
+type ParsedData = { title: string }
+
+let read (fixtureName: string) : string =
+    File.ReadAllText(Path.Combine("../../..", "fixtures", fixtureName))
 
 [<Tests>]
 let tests =
-    test "A simple test" {
-        let subject = Say.hello "World"
-        Expect.equal subject "Hello World" "The strings should equal"
-    }
+    testList
+        "section-mattr"
+        [
+
+          test "should return a file object" {
+              Expect.equal
+                  (NewMattr.sections "")
+                  { Mattr.content = ""
+                    Mattr.sections = [||] }
+                  "should be equal"
+
+              Expect.equal
+                  (NewMattr.sections "foo")
+                  { Mattr.content = "foo"
+                    Mattr.sections = [||] }
+                  "should be equal"
+          }
+
+          test "should correctly parse non-sections" {
+              Expect.equal
+                  (NewMattr.sections "foo\n---\nbar")
+                  { Mattr.content = "foo\n---\nbar"
+                    Mattr.sections = [||] }
+                  "should be equal"
+
+              Expect.equal
+                  (NewMattr.sections "foo\n---\nbar\n---")
+                  { Mattr.content = "foo\n---\nbar\n---"
+                    Mattr.sections = [||] }
+                  "should be equal"
+          }
+
+          test "should parse front-matter without language" {
+              Expect.equal
+                  (NewMattr.sections "---\ntitle: bar\n---\n\nfoo")
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = ""
+                           MattrSection.data = "title: bar"
+                           MattrSection.content = "\nfoo" } |] }
+                  "should be equal"
+
+              Expect.equal
+                  (NewMattr.sections "---\nfoo\n---\nbar")
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = ""
+                           MattrSection.data = "foo"
+                           MattrSection.content = "bar" } |] }
+                  "should be equal"
+          }
+
+          test "should parse front-matter with language" {
+              let input: string = "---json\n{\"title\": \"bar\"}\n---\n\nfoo"
+
+              Expect.equal
+                  (NewMattr.sections input)
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = "json"
+                           MattrSection.data = "{\"title\": \"bar\"}"
+                           MattrSection.content = "\nfoo" } |] }
+                  "should be equal"
+          }
+
+          test "should parse a section" {
+              let input: string =
+                  "---\ntitle: bar\n---\n\nfoo\n---one\ntitle: One\n---\nThis is one"
+
+              Expect.equal
+                  (NewMattr.sections input)
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = ""
+                           MattrSection.data = "title: bar"
+                           MattrSection.content = "\nfoo" }
+                         { MattrSection.key = "one"
+                           MattrSection.data = "title: One"
+                           MattrSection.content = "This is one" } |] }
+                  "should be equal"
+          }
+
+          test "should use custom section_delimiter" {
+              let input: string =
+                  "~~~\ntitle: bar\n~~~\n\nfoo\n~~~one\ntitle: One\n~~~\nThis is one"
+
+              Expect.equal
+                  (NewMattr.sections (input, { MattrOption1.section_delimiter = "~~~" }))
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = ""
+                           MattrSection.data = "title: bar"
+                           MattrSection.content = "\nfoo" }
+                         { MattrSection.key = "one"
+                           MattrSection.data = "title: One"
+                           MattrSection.content = "This is one" } |] }
+                  "should be equal"
+          }
+
+          test "should use a custom parser on sections" {
+              let input =
+                  "---json\n{\"title\": \"bar\"}\n---\n\nfoo\n---json\n{\"title\": \"One\"}\n---\nThis is one"
+
+              let parse (section: MattrSection<string>) (_: MattrSection<ParsedData> array) : MattrSection<ParsedData> =
+                  Mattrial.appendData (JsonSerializer.Deserialize<ParsedData>(section.data)) section
+
+              Expect.equal
+                  (NewMattr.sections (input, parse))
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = "json"
+                           MattrSection.data = { ParsedData.title = "bar" }
+                           MattrSection.content = "\nfoo" }
+                         { MattrSection.key = "json"
+                           MattrSection.data = { ParsedData.title = "One" }
+                           MattrSection.content = "This is one" } |] }
+                  "should be equal"
+          }
+
+          test "should parse multiple sections" {
+              let input = read "multiple.md"
+
+              Expect.equal
+                  (NewMattr.sections input)
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = ""
+                           MattrSection.data = "title: bar"
+                           MattrSection.content = "\nfoo\n" }
+                         { MattrSection.key = "one"
+                           MattrSection.data = "title: One"
+                           MattrSection.content = "This is one\n" }
+                         { MattrSection.key = "two"
+                           MattrSection.data = "title: Two"
+                           MattrSection.content = "This is two\n" } |] }
+                  "should be equal"
+          }
+
+          test "should not parse non-sections" {
+              let input = read "hr.md"
+
+              Expect.equal
+                  (NewMattr.sections input)
+                  { Mattr.content = ""
+                    Mattr.sections =
+                      [| { MattrSection.key = "yaml"
+                           MattrSection.data = "title: I'm front matter"
+                           MattrSection.content =
+                             "\nThis page has front matter that should be parsed before the sections.\n" }
+                         { MattrSection.key = "aaa"
+                           MattrSection.data = "title: First section"
+                           MattrSection.content = "\nSection one.\n" }
+                         { MattrSection.key = "bbb"
+                           MattrSection.data = "title: Non-section horizontal rules"
+                           MattrSection.content = "\nPart 1.\n\n---\n\nPart 2.\n\n---\n\nPart 3.\n" }
+                         { MattrSection.key = "ccc"
+                           MattrSection.data = "title: Third section"
+                           MattrSection.content = "\nSection three.\n" } |] }
+                  "should be equal"
+          }
+
+          ]
 
 [<EntryPoint>]
 let main args = runTestsWithCLIArgs [] args tests
--- SectionMattr/Library.fs
+++ SectionMattr/Library.fs
@@ -1,4 +1,189 @@
-﻿namespace SectionMattr
+﻿module SectionMattr
 
-module Say =
-    let hello name = sprintf "Hello %s" name
+open System.Text.RegularExpressions
+
+[<Struct>]
+type MattrSection<'p> =
+    { key: string
+      data: 'p
+      content: string }
+
+[<Struct>]
+type Mattr<'p> =
+    { content: string
+      sections: MattrSection<'p> array }
+
+type Parser<'p> = MattrSection<string> -> MattrSection<'p> array -> MattrSection<'p>
+
+[<Struct>]
+type MattrOption1 = { section_delimiter: string }
+
+[<Struct>]
+type MattrOption2<'p> = { parse: Parser<'p> }
+
+[<Struct>]
+type MattrOption<'p> =
+    { section_delimiter: string
+      parse: Parser<'p> }
+
+module Mattrial =
+    let defaultOption<'p> (parser: Parser<'p>) : MattrOption<'p> =
+        { MattrOption.section_delimiter = "---"
+          MattrOption.parse = parser }
+
+    let createSection () : MattrSection<string> =
+        { MattrSection.key = ""
+          MattrSection.data = ""
+          MattrSection.content = "" }
+
+    let toObject<'p> (input: string) : Mattr<'p> =
+        { Mattr.content = input
+          Mattr.sections = [||] }
+
+    let identity (section: MattrSection<string>) (_: MattrSection<string> array) : MattrSection<string> = section
+
+    let getKey (dataLine: string option) (delim: string) : string =
+        match dataLine with
+        | None -> ""
+        | Some "" -> ""
+        | Some(dataLine: string) -> dataLine.Substring(delim.Length).Trim()
+
+    let isDelimiter (line: string) (delim: string) : bool =
+        if (not (line.StartsWith delim)) then
+            false
+        else if (line.Length > delim.Length && line[delim.Length] = delim[delim.Length - 1]) then
+            false
+        else
+            true
+
+    let appendData<'p> (data: 'p) (section: MattrSection<string>) : MattrSection<'p> =
+        let parsed: MattrSection<'p> =
+            { MattrSection.key = section.key
+              MattrSection.content = section.content
+              MattrSection.data = data }
+
+        parsed
+
+    let parse<'p> (entity: Mattr<'p>) (opts: MattrOption<'p>) =
+        let delim: string = opts.section_delimiter
+        let lines: string array = Regex.Split(entity.content, @"\r?\n")
+
+        let mutable _entity: Mattr<'p> = entity
+        let mutable sections: MattrSection<'p> array option = None
+        let mutable content: string array = [||]
+        let mutable section: MattrSection<string> = createSection ()
+        let mutable stack: string array = [||]
+
+        let initSections (contentStr: string) =
+            _entity <- { _entity with content = contentStr }
+            sections <- Some [||]
+            content <- [||]
+
+        let closeSection (contentStr: string) =
+            if (stack.Length <> 0) then
+                section <-
+                    { section with
+                        key = getKey (stack |> Array.tryItem 0) delim
+                        content = contentStr }
+
+                sections <-
+                    match sections with
+                    | Some(sections: MattrSection<'p> array) ->
+                        Some(Array.append sections [| (opts.parse section sections) |])
+                    | None -> None
+
+                section <- createSection ()
+                content <- [||]
+                stack <- [||]
+
+        for i in 0 .. lines.Length - 1 do
+            let line = lines[i]
+            let len = stack.Length
+            let ln = line.Trim()
+
+            let cond1 = isDelimiter ln delim
+            let cond2 = ln.Length = 3 && i <> 0
+            let cond3 = len = 0 || len = 2
+            let cond4 = Option.isNone sections
+            let cond5 = len = 2
+
+            if (cond1 && cond2 && cond3) then
+                content <- Array.append content [| line |]
+            else if (cond1 && cond2) then
+                stack <- Array.append stack [| ln |]
+
+                section <-
+                    { section with
+                        MattrSection.data = content |> String.concat "\n" }
+
+                content <- [||]
+            else if (cond1 && cond4) then
+                initSections (content |> String.concat "\n")
+                stack <- Array.append stack [| ln |]
+            else if (cond1 && cond5) then
+                closeSection (content |> String.concat "\n")
+                stack <- Array.append stack [| ln |]
+            else
+                content <- Array.append content [| line |]
+
+        if (Option.isNone sections) then
+            initSections (content |> String.concat "\n")
+        else
+            closeSection (content |> String.concat "\n")
+
+        match sections with
+        | Some(sections: MattrSection<'p> array) -> { _entity with sections = sections }
+        | None -> _entity
+
+[<Struct>]
+type NewMattr =
+    { name: string }
+
+    static member sections(input: string) : Mattr<string> =
+        let file: Mattr<string> = Mattrial.toObject input
+        let option: MattrOption<string> = Mattrial.defaultOption Mattrial.identity
+        Mattrial.parse file option
+
+    static member sections<'p>(input: string, parser: Parser<'p>) : Mattr<'p> =
+        let file: Mattr<'p> = Mattrial.toObject input
+        let option: MattrOption<'p> = Mattrial.defaultOption parser
+        Mattrial.parse file option
+
+    static member sections(input: string, option: MattrOption1) : Mattr<string> =
+        let file: Mattr<string> = Mattrial.toObject input
+
+        let option: MattrOption<string> =
+            { MattrOption.section_delimiter = option.section_delimiter
+              MattrOption.parse = Mattrial.identity }
+
+        Mattrial.parse file option
+
+    static member sections<'p>(input: string, option: MattrOption2<'p>) : Mattr<'p> =
+        let file: Mattr<'p> = Mattrial.toObject input
+        let option: MattrOption<'p> = Mattrial.defaultOption option.parse
+        Mattrial.parse file option
+
+    static member sections<'p>(input: string, option: MattrOption<'p>) : Mattr<'p> =
+        let file: Mattr<'p> = Mattrial.toObject input
+        Mattrial.parse file option
+
+    static member sections(entity: Mattr<string>) : Mattr<string> =
+        let option: MattrOption<string> = Mattrial.defaultOption Mattrial.identity
+        Mattrial.parse entity option
+
+    static member sections<'p>(entity: Mattr<'p>, parser: Parser<'p>) : Mattr<'p> =
+        let option: MattrOption<'p> = Mattrial.defaultOption parser
+        Mattrial.parse entity option
+
+    static member sections(entity: Mattr<string>, option: MattrOption1) : Mattr<string> =
+        let option: MattrOption<string> =
+            { MattrOption.section_delimiter = option.section_delimiter
+              MattrOption.parse = Mattrial.identity }
+
+        Mattrial.parse entity option
+
+    static member sections<'p>(entity: Mattr<'p>, option: MattrOption2<'p>) : Mattr<'p> =
+        let option: MattrOption<'p> = Mattrial.defaultOption option.parse
+        Mattrial.parse entity option
+
+    static member sections<'p>(entity: Mattr<'p>, option: MattrOption<'p>) : Mattr<'p> = Mattrial.parse entity option
